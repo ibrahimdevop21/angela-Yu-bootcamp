0c50ba80905d476686eba342735a2a39205c8a0f
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-01-12 16:11:22 +0200

    added the ejs file

---
+++ back-end/ejs/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const app = express();
+const port = 2000;
+
+app.get("/", (req, res) => {
+  const today = new Date();
+  const day = today.getDay();
+
+  let type = "a weekday";
+  let adv = "it's time to work hard";
+  
+  if (day === 0 || day === 6) {
+    type = "the weekend";
+    adv = "it's time to enjoy and have some fun";
+  } else {
+    type = "a weekday";
+    adv = "it's time to work hard"
+  }
+  res.render("index.ejs", {
+    dayType: type,
+    advice: adv,
+  });
+});
+//! how we pass data from our server to template file , using ejs 
+
+app.listen(port, () => {
+  console.log(`server is up and running on port ${port}`);
+});
