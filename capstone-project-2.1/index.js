55a140c97e042d66fc217ea615471f8278627a26
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-01-24 23:14:53 +0200

    built the main structure for the blog post

---
+++ capstone-project-2.1/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/boom", (req, res) => {
+  const issuValue = req.body.issu;
+  const postValue = req.body.post;
+
+  console.log(`Issue: ${issuValue}, Post: ${postValue}`);
+  res.render("boom.ejs", { issuValue, postValue });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
+});
