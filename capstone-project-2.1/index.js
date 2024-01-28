16f432b5df2fead140c45f9c923dcbb844a33836
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-01-28 05:15:34 +0200

    initial commit basic structure and styles

--- capstone-project-2.1/index.js
+++ capstone-project-2.1/index.js
@@ -1,25 +1,23 @@
-// TODO: 1.viewing the post, 2.editing the post 3.deleting the post
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+
+// Use import.meta.url to get the current file's URL and extract the directory
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.post("/boom", (req, res) => {
-  const issuValue = req.body.issu;
-  const postValue = req.body.post;
-
-  console.log(`Issue: ${issuValue}, Post: ${postValue}`);
-  res.render("boom.ejs", { issuValue, postValue });
-});
-
 app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
+  console.log(`Server is up and running on port: ${port}`);
 });
---
+++ capstone-project-2.1/index.md
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Linux Mint blog</title>
+    <link rel="stylesheet" href="./css/styles.css" />
+  </head>
+  <body>
+    <main>
+      <div id="header">
+        <nav id="nav-bar">
+          <li class="nav-link"><a href="#home">Home</a></li>
+          <li class="nav-link"><a href="#blog">Blog</a></li>
+        </nav>
+      </div>
+      <div id="logo">
+        <img src="./images/Logo_Linux_Mint.png" alt="" />
+      </div>
+
+      <div id="footer">
+        <nav id="bottom-nav">
+          <li class="nav-link"><a href="#home">Home</a></li>
+          <li class="nav-link"><a href="#blog">Blog</a></li>
+        </nav>
+      </div>
+    </main>
+    <script src="../index.js"></script>
+  </body>
+</html>
