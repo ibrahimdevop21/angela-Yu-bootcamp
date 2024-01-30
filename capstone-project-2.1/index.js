6f49ee6928cc9b536912e85d71fb41d8ba6450e3
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2024-01-30 12:16:46 +0200

    its working

--- capstone-project-2.1/index.js
+++ capstone-project-2.1/index.js
@@ -14,23 +14,44 @@ const __dirname = path.dirname(__filename);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// Array to store posts
+const posts = [];
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { posts });
 });
 
 app.post("/index", (req, res) => {
   const issueValue = req.body.issue;
   const postValue = req.body.post;
 
-  console.log(`Issue: ${issueValue}, Post: ${postValue}`);
+  // Generate a unique ID for the new post
+  const postId = generateUniqueId();
 
-  res.render("index.ejs", { issueValue, postValue });
+  // Create a new post object
+  const newPost = {
+    id: postId,
+    issue: issueValue,
+    post: postValue,
+  };
+
+  // Add the new post to the array
+  posts.push(newPost);
+
+  console.log(
+    `New Post created with ID ${postId}: ${issueValue}, ${postValue}`
+  );
+
+  // Render the index page with the updated posts array
+  res.render("index.ejs", { posts });
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong!");
-});
+// Implement the generateUniqueId function
+function generateUniqueId() {
+  return Math.random().toString(36).substr(2, 9);
+}
+
+// ... (existing code)
 
 app.listen(port, () => {
   console.log(`Server is up and running on port: ${port}`);
